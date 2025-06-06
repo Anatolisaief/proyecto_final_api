5cc17d88193606f06e9bd15a4535879464594089
anatolisaief <anatolisaief@gmail.com>
2025-06-06 10:27:10 +0000

    version1.0

---
+++ README.md
@@ -0,0 +1,39 @@
+# 🔮 Tarot Cards App
+
+Una aplicación web interactiva que simula una lectura de cartas de tarot. Puedes hacer una pregunta y obtener una carta al azar, o ver todas las cartas disponibles con sus significados.
+
+## Descripción
+
+La aplicación obtiene cartas desde la API pública [Tarot API](https://tarotapi.dev) y muestra su significado y descripción. Además, combina esas cartas con imágenes personalizadas guardadas localmente. Para esto, utiliza un array en un archivo .js donde se define cada carta junto con la ruta de su imagen correspondiente desde la carpeta tarotdeck.
+
+## Tecnologías utilizadas
+
+- HTML5
+- CSS3 (Responsive y estilizado con fuentes místicas)
+- JavaScript (Vanilla)
+- [Tarot API](https://tarotapi.dev) para la información de las cartas
+
+## Funcionalidades
+
+- Mostrar una carta de tarot aleatoria al hacer una pregunta.
+- Ver todas las cartas disponibles.
+- Mostrar imágenes personalizadas para cada carta (ubicadas en `/tarotdeck`).
+- Interfaz responsiva para dispositivos móviles.
+
+# Estructura de archivos
+
+tarot-app/
+│
+├── index.html
+├── style.css
+├── script.js
+├── tarotdeck/
+│ ├── thefool.jpeg
+│ ├── themagician.jpeg
+│ └── ...otras imágenes
+
+## Cómo usar
+
+1. Clona el repositorio.
+2. Abre `index.html` en tu navegador.
+3. Escribe una pregunta y presiona "Ask" o haz clic en "Show all cards".
---
+++ script.js
@@ -0,0 +1,355 @@
+
+const tarotCardsImages = [
+    {
+        name: "The Fool", image: "tarotdeck/thefool.jpeg",
+    },
+    {
+        name: "The Magician", image: "tarotdeck/themagician.jpeg",
+    },
+    {
+        name: "The High Priestess", image: "tarotdeck/thehighpriestess.jpeg",
+    },
+    {
+        name: "The Empress", image: "tarotdeck/theempress.jpeg",
+    },
+    {
+        name: "The Emperor", image: "tarotdeck/theemperor.jpeg",
+    },
+    {
+        name: "The Hierophant", image: "tarotdeck/thehierophant.jpeg",
+    },
+    {
+        name: "The Lovers", image: "tarotdeck/TheLovers.jpg",
+    },
+    {
+        name: "The Chariot", image: "tarotdeck/thechariot.jpeg",
+    },
+    {
+        name: "Fortitude", image: "tarotdeck/thestrength.jpeg",
+    },
+    {
+        name: "The Hermit", image: "tarotdeck/thehermit.jpeg",
+    },
+    {
+        name: "Wheel Of Fortune", image: "tarotdeck/wheeloffortune.jpeg",
+    },
+    {
+        name: "Justice", image: "tarotdeck/justice.jpeg",
+    },
+    {
+        name: "The Hanged Man", image: "tarotdeck/thehangedman.jpeg",
+    },
+    {
+        name: "Death", image: "tarotdeck/death.jpeg",
+    },
+    {
+        name: "Temperance", image: "tarotdeck/temperance.jpeg",
+    },
+    {
+        name: "The Devil", image: "tarotdeck/thedevil.jpeg",
+    },
+    {
+        name: "The Tower", image: "tarotdeck/thetower.jpeg",
+    },
+    {
+        name: "The Star", image: "tarotdeck/thestar.jpeg",
+    },
+    {
+        name: "The Moon", image: "tarotdeck/themoon.jpeg",
+    },
+    {
+        name: "The Sun", image: "tarotdeck/thesun.jpeg",
+    },
+    {
+        name: "The Last Judgment", image: "tarotdeck/judgement.jpeg",
+    },
+    {
+        name: "The World", image: "tarotdeck/theworld.jpeg",
+    },
+    {
+        name: "Ace of Cups", image: "tarotdeck/aceofcups.jpeg",
+    },
+    {
+        name: "Two of Cups", image: "tarotdeck/twoofcups.jpeg",
+    },
+    {
+        name: "Three of Cups", image: "tarotdeck/threeofcups.jpeg",
+    },
+    {
+        name: "Four of Cups", image: "tarotdeck/fourofcups.jpeg",
+    },
+    {
+        name: "Five of Cups", image: "tarotdeck/fiveofcups.jpeg",
+    },
+    {
+        name: "Six of Cups", image: "tarotdeck/sixofcups.jpeg",
+    },
+    {
+        name: "Seven of Cups", image: "tarotdeck/sevenofcups.jpeg",
+    },
+    {
+        name: "Eight of Cups", image: "tarotdeck/eightofcups.jpeg",
+    },
+    {
+        name: "Nine of Cups", image: "tarotdeck/nineofcups.jpeg",
+    },
+    {
+        name: "Ten of Cups", image: "tarotdeck/tenofcups.jpeg",
+    },
+    {
+        name: "Page of Cups", image: "tarotdeck/pageofcups.jpeg",
+    },
+    {
+        name: "Knight of Cups", image: "tarotdeck/knightofcups.jpeg",
+    },
+    {
+        name: "Queen of Cups", image: "tarotdeck/queenofcups.jpeg",
+    },
+    {
+        name: "King of Cups", image: "tarotdeck/kingofcups.jpeg",
+    },
+    {
+        name: "Ace of Pentacles", image: "tarotdeck/aceofpentacles.jpeg",
+    },
+    {
+        name: "Two of Pentacles", image: "tarotdeck/twoofpentacles.jpeg",
+    },
+    {
+        name: "Three of Pentacles", image: "tarotdeck/threeofpentacles.jpeg",
+    },
+    {
+        name: "Four of Pentacles", image: "tarotdeck/fourofpentacles.jpeg",
+    },
+    {
+        name: "Five of Pentacles", image: "tarotdeck/fiveofpentacles.jpeg",
+    },
+    {
+        name: "Six of Pentacles", image: "tarotdeck/sixofpentacles.jpeg",
+    },
+    {
+        name: "Seven of Pentacles", image: "tarotdeck/sevenofpentacles.jpeg",
+    },
+    {
+        name: "Eight of Pentacles", image: "tarotdeck/eightofpentacles.jpeg",
+    },
+    {
+        name: "Nine of Pentacles", image: "tarotdeck/nineofpentacles.jpeg",
+    },
+    {
+        name: "Ten of Pentacles", image: "tarotdeck/tenofpentacles.jpeg",
+    },
+    {
+        name: "Page of Pentacles", image: "tarotdeck/pageofpentacles.jpeg",
+    },
+    {
+        name: "Knight of Pentacles", image: "tarotdeck/knightofpentacles.jpeg",
+    },
+    {
+        name: "Queen of Pentacles", image: "tarotdeck/queenofpentacles.jpeg",
+    },
+    {
+        name: "King of Pentacles", image: "tarotdeck/kingofpentacles.jpeg",
+    },
+    {
+        name: "Ace of Swords", image: "tarotdeck/aceofswords.jpeg",
+    },
+    {
+        name: "Two of Swords", image: "tarotdeck/twoofswords.jpeg",
+    },
+    {
+        name: "Three of Swords", image: "tarotdeck/threeofswords.jpeg",
+    },
+    {
+        name: "Four of Swords", image: "tarotdeck/fourofswords.jpeg",
+    },
+    {
+        name: "Five of Swords", image: "tarotdeck/fiveofswords.jpeg",
+    },
+    {
+        name: "Six of Swords", image: "tarotdeck/sixofswords.jpeg",
+    },
+    {
+        name: "Seven of Swords", image: "tarotdeck/sevenofswords.jpeg",
+    },
+    {
+        name: "Eight of Swords", image: "tarotdeck/eightofswords.jpeg",
+    },
+    {
+        name: "Nine of Swords", image: "tarotdeck/nineofswords.jpeg",
+    },
+    {
+        name: "Ten of Swords", image: "tarotdeck/tenofswords.jpeg",
+    },
+    {
+        name: "Page of Swords", image: "tarotdeck/pageofswords.jpeg",
+    },
+    {
+        name: "Knight of Swords", image: "tarotdeck/knightofswords.jpeg",
+    },
+    {
+        name: "Queen of Swords", image: "tarotdeck/queenofswords.jpeg",
+    },
+    {
+        name: "King of Swords", image: "tarotdeck/kingofswords.jpeg",
+    },
+    {
+        name: "Ace of Wands", image: "tarotdeck/aceofwands.jpeg",
+    },
+    {
+        name: "Two of Wands", image: "tarotdeck/twoofwands.jpeg",
+    },
+    {
+        name: "Three of Wands", image: "tarotdeck/threeofwands.jpeg",
+    },
+    {
+        name: "Four of Wands", image: "tarotdeck/fourofwands.jpeg",
+    },
+    {
+        name: "Five of Wands", image: "tarotdeck/fiveofwands.jpeg",
+    },
+    {
+        name: "Six of Wands", image: "tarotdeck/sixofwands.jpeg",
+    },
+    {
+        name: "Seven of Wands", image: "tarotdeck/sevenofwands.jpeg",
+    },
+    {
+        name: "Eight of Wands", image: "tarotdeck/eightofwands.jpeg",
+    },
+    {
+        name: "Nine of Wands", image: "tarotdeck/nineofwands.jpeg",
+    },
+    {
+        name: "Ten of Wands", image: "tarotdeck/tenofwands.jpeg",
+    },
+    {
+        name: "Page of Wands", image: "tarotdeck/pageofwands.jpeg",
+    },
+    {
+        name: "Knight of Wands", image: "tarotdeck/knightofwands.jpeg",
+    },
+    {
+        name: "Queen of Wands", image: "tarotdeck/queenofwands.jpeg",
+    },
+    {
+        name: "King of Wands", image: "tarotdeck/kingofwands.jpeg",
+    },
+];
+
+
+const container = document.querySelector(".cards-container");
+const form = document.getElementById("tarot-form");
+const input = document.getElementById("texto");
+const showAllBtn = document.getElementById("show-all");
+
+let tarotCards = []; // Se guarda aquí la data de la API
+
+async function fetchData() {
+    try {
+
+        const response = await fetch("https://tarotapi.dev/api/v1/cards");
+        const data = await response.json();
+        tarotCards = data.cards; // Guardamos para uso posterior
+        console.log(data);
+        /*   data.cards.forEach((apiCard, index) => {
+  
+              container.innerHTML += "";
+              container.innerHTML += `
+                      
+                          <div class="card-header">
+                              <h3>${apiCard.name}</h3>
+                              <div class="image-container"id="img-${index}"></div>
+                          </div>
+                          <div class="content">
+                              <p>${apiCard.meaning_up}</p>
+                              <p>${apiCard.desc}</p>
+                          </div>
+                          
+                      </div>
+                       
+                      `;
+  
+              const containerImages = document.getElementById(`img-${index}`);
+  
+              tarotCardsImages.forEach(cardImage => {
+                  if (apiCard.name.toLowerCase() === cardImage.name.toLowerCase()) {
+                      containerImages.innerHTML += `
+                          <img src="${cardImage.image}" alt="${cardImage.name}">
+                      `;
+                  }
+              });
+   
+          });*/
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Evita recargar la página
+    const question = input.value.trim();
+    if (question) {
+        showRandomCard();
+        input.value = ""; // Limpia el input
+    }
+});
+
+showAllBtn.addEventListener("click", function () {
+    showAllCards();
+});
+
+function showRandomCard() {
+
+    if (tarotCards.length === 0) return;// // Si no hay cartas, no hacemos nada
+
+    const randomIndex = Math.floor(Math.random() * tarotCards.length);
+    const card = tarotCards[randomIndex];
+    const image = tarotCardsImages.find(img => img.name.toLowerCase() === card.name.toLowerCase());
+
+
+    container.innerHTML = `
+        <div class="card">
+            <div class="card-header">
+                <h3>${card.name}</h3>
+                <div class="image-container">
+                    <img src="${image.image}" alt="${card.name}">
+                </div>
+            </div>
+            <div class="content">
+                    <h4>Meaning:</h4>
+                    <p>${card.meaning_up}</p>
+                    <h4>Description:</h4>
+                    <p>${card.desc}</p>
+            </div>
+        </div>
+    `;
+}
+function showAllCards() {
+    if (tarotCards.length === 0) return; // Si no hay cartas, no hacemos nada
+    container.innerHTML = ""; // Limpia el contenedor antes de mostrar todas las cartas
+    tarotCards.forEach(card => {
+
+        const image = tarotCardsImages.find(img => img.name.toLowerCase() === card.name.toLowerCase());
+
+
+        container.innerHTML += `
+            <div class="card">
+                <div class="card-header">
+                    <h3>${card.name}</h3>
+                    <div class="image-container">
+                        <img src="${image.image}" alt="${card.name}">
+                    </div>
+                </div>
+                <div class="content">
+                    <h4>Meaning:</h4>
+                    <p>${card.meaning_up}</p>
+                    <h4>Description:</h4>
+                    <p>${card.desc}</p>
+                </div>
+            </div>
+        `;
+    });
+}
+fetchData();
+
